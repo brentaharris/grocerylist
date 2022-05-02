3d225e5894d963fc94a0a88e3256aab04714fbf6
brentaharris <brentaustinharris@gmail.com>
2022-05-02 12:28:00 -0400

    added list items and check functionality

--- src/Content.js
+++ src/Content.js
@@ -1,8 +1,52 @@
 
+import { useState } from 'react';
+import { FaTrashAlt } from 'react-icons/fa'; 
+
 function Content() {
+  const [ items, setItems ] =  useState([
+    {
+      id: 1,
+      checked: false,
+      item: 'One half pound bag of Coca Covered Almonds Unsalted'
+    },
+    {
+      id: 2,
+      checked: false,
+      item: 'Item 2'
+    },
+    {
+      id: 3,
+      checked: false,
+      item: 'Item 3'
+    }
+  ]);
+
+  const handleCheck = (id) => {
+    const listItems = items.map((item) => item.id === id ? {
+      ...item, checked: !item.checked } : item);
+      setItems(listItems);
+      localStorage.setItem('shoppinglist', JSON.stringify(listItems))
+  }
+
   return (
     <main>
-        Hello
+      <ul>
+        { items.map((item) => (
+        <li className='item' key={item.id}>
+          <input 
+            id="list" 
+            type="checkbox"  
+            onChange={() => handleCheck(item.id)}
+            checked={item.checked}
+          />
+          <label 
+            htmlFor='list'
+            // style={(item.checked) ? { textDecoration: 'line-though'} : null}
+            // onDoubleClick={() => handleCheck(item.id)}
+          >{item.item}</label>
+          <FaTrashAlt role="button" tabIndex="0" />
+        </li>))}
+      </ul>  
     </main>
   )
 }
