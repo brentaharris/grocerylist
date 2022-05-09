a12915385cab2d5f47e5d33e4f55f3edf4776ad8
brentaharris <brentaharris@users.noreply.github.com>
2022-05-09 14:45:37 -0400

    added state and effect

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
-import Header from './Header';
-import SearchItem from './SearchItem';
-import AddItem  from './AddItem';
-import Content from './Content';
-import Footer from './Footer';
-import { useState } from 'react';
-
+import Header from "./Header";
+import SearchItem from "./SearchItem";
+import AddItem from "./AddItem";
+import Content from "./Content";
+import Footer from "./Footer";
+import { useState, useEffect } from "react";
 
 function App() {
+  const API_URL = "http://localhost:3500/items";
 
-  const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')));
-  const [newItem, setNewItem] = useState('');
-  const [search, setSearch] = useState('');
+  const [items, setItems] = useState([]);
+  const [newItem, setNewItem] = useState("");
+  const [search, setSearch] = useState("");
+  const [fetchError, setFetchError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
-  const setAndSaveItems = (newItems) => {
-    setItems(newItems);
-    localStorage.setItem("shoppinglist", JSON.stringify(newItems));
-  }
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const response = await fetch(API_URL);
+        const listItems = await response.json();
+        if (!response.ok) throw Error("Did not recieve expected data");
+        setItems(listItems);
+        setFetchError(null);
+      } catch (err) {
+        setFetchError(err.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    setTimeout(() => {
+      fetchItems();
+    }, 2000);
+  }, []);
 
   const addItem = (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
     const myNewItem = { id, checked: false, item };
     const listItems = [...items, myNewItem];
-    setAndSaveItems(listItems);
-  }
+    setItems(listItems);
+  };
 
   const handleCheck = (id) => {
     const listItems = items.map((item) =>
@@ -33,40 +49,44 @@ function App() {
           }
         : item
     );
-    setAndSaveItems(listItems);
+    setItems(listItems);
   };
 
   const handleDelete = (id) => {
     const listItems = items.filter((item) => item.id !== id);
-    setAndSaveItems(listItems);
+    setItems(listItems);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if(!newItem) return;
+    if (!newItem) return;
     //addItem
     addItem(newItem);
-    setNewItem('');
-  }
-
+    setNewItem("");
+  };
 
   return (
     <div className="App">
-      <Header title='Grocery List' />
-      <AddItem 
+      <Header title="Grocery List" />
+      <AddItem
         newItem={newItem}
         setNewItem={setNewItem}
         handleSubmit={handleSubmit}
       />
-      <SearchItem 
-        search={search}
-        setSearch={setSearch}
-      />
-      <Content 
-        items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
-        handleCheck={handleCheck} 
-        handleDelete={handleDelete} 
-      />
+      <SearchItem search={search} setSearch={setSearch} />
+      <main>
+        {isLoading && <p>Loading Items...</p>}
+        {fetchError && <p style={{ color: "red" }}>{`Error: ${fetchError}`}</p>}
+        {!fetchError && !isLoading && (
+          <Content
+            items={items.filter((item) =>
+              item.item.toLowerCase().includes(search.toLowerCase())
+            )}
+            handleCheck={handleCheck}
+            handleDelete={handleDelete}
+          />
+        )}
+      </main>
       <Footer length={items.length} />
     </div>
   );
--- src/Content.js
+++ src/Content.js
@@ -1,21 +1,19 @@
-
 import ItemList from "./ItemList";
 
-const Content = ({ items, handleCheck, handleDelete}) => {
-  
+const Content = ({ items, handleCheck, handleDelete }) => {
   return (
-    <main>
+    <>
       {items.length ? (
-        <ItemList 
-          items={items} 
-          handleCheck={handleCheck} 
-          handleDelete={handleDelete} 
+        <ItemList
+          items={items}
+          handleCheck={handleCheck}
+          handleDelete={handleDelete}
         />
       ) : (
         <p style={{ marginTop: "2rem" }}>Your list is empty.</p>
       )}
-    </main>
+    </>
   );
-}
+};
 
 export default Content;
