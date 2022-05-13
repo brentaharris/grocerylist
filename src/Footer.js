3a631bb28e61c3fb9ef18a3df55b623e7e6e9beb
brentaharris <brentaustinharris@gmail.com>
2022-05-13 11:21:19 -0400

    removed date warning for build

--- src/Footer.js
+++ src/Footer.js
@@ -1,6 +1,6 @@
 
 const Footer = ( {length }) => {
-    const today = new Date();
+    // const today = new Date();
   return (
     <footer>
         <p>{length} List {length === 1 ? 'item' : 'items' }</p>
